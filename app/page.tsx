1e7ea13e667ca0dc93dfce3b22cecb2b61235c2a
Raphael David <raphaelruizd@gmail.com>
2026-04-30 22:07:22 +1000

    Build homepage table populated with API data and shadcn table component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,51 @@
-import Image from "next/image";
+"use client";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useEffect } from "react";
+import { fetchItems } from "@/lib/itemsSlice";
+import { useRouter } from "next/navigation";
+
+
 export default function Home() {
+  const dispatch =  useAppDispatch();
+  const router = useRouter();
+  const { data, status } = useAppSelector((state) => state.items);
+  console.log("Items state:", { data, status });
+
+  useEffect(() => {
+    console.log("Dispatching fetchItems...");
+    dispatch(fetchItems());
+    console.log("Fetched items:", data);
+  }, [dispatch]);
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableCaption>A list of items from API endpoint</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="w-25">GUID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Path</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
-            </TableRow>
+
+            {data.map((item) => (
+              <TableRow
+                key={item.guid}
+                className="cursor-pointer hover:bg-zinc-100 hover:text-black"
+                onClick={() => {
+                  console.log("Navigating to item:", item.guid);
+                  router.push(`/items/${item.guid}`);
+                }}
+              >
+                <TableCell className="font-medium">{item.guid}</TableCell>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.path.join(" / ")}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </main>
